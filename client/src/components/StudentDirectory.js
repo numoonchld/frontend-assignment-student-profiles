cacd91977d6fb924fca687d53659d036d0b82299
numoonchld <raghavendra.saralaya@gmail.com>
2021-02-28 08:15:55 +0530

    name search works fine mostly

--- client/src/components/StudentDirectory.js
+++ client/src/components/StudentDirectory.js
@@ -4,6 +4,8 @@ import Student from './Student'
 const StudentDirectory = () => {
 
     const [students, setStudents] = useState([])
+    const [nameSearchTerm, setNameSearchTerm] = useState('')
+    // const [tagSearchTerm, setTagSearchTerm] = useState('')
 
     const getData = async () => {
         const response = await fetch('https://api.hatchways.io/assessment/students')
@@ -16,10 +18,14 @@ const StudentDirectory = () => {
         getData()
     }, [])
 
+    const studentsFiltered = students.filter(student => (student.firstName + student.lastName).toLowerCase().includes(nameSearchTerm.toLowerCase()))
+
     return <Fragment>
-        <div className="card my-5 rounded w-75 mx-auto" id='directory'>
+        <div className="card my-5 p-3 rounded w-75 mx-auto" id='directory'>
+            <input className='p-2 my-1 w-100' type='text' placeholder='Search by name' value={nameSearchTerm} onChange={(event)=>setNameSearchTerm(event.target.value)}/>
+            <input className='p-2 my-1 w-100' type='text' placeholder='Search by tag' />
             <ul className="list-group list-group-flush">
-                {students.map(student => <li className="list-group-item" key={student.id}> <Student key={student.id} {...student} /> </li>)}
+                {studentsFiltered.map(student => <li className="list-group-item" key={student.id}> <Student key={student.id} {...student} /> </li>)}
             </ul>
         </div>
     </Fragment>
