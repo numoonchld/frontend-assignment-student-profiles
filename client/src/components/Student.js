345e4e9d2ed8d61395abcab1fa71d14a817cf189
numoonchld <raghavendra.saralaya@gmail.com>
2021-03-01 09:35:20 +0530

    part 5 mostly complete

--- client/src/components/Student.js
+++ client/src/components/Student.js
@@ -1,48 +1,126 @@
 import React, { Fragment, useState } from 'react'
 
-const Student = ({ firstName, lastName, email, company, skill, pic, grades }) => {
+const Student = ({ firstName, lastName, email, company, skill, pic, grades, tagSearchTerm }) => {
 
     const addReducer = (arrayToSum) => {
         return arrayToSum.reduce((a, b) => (parseInt(a) + parseInt(b)))
     }
 
     const [showGrades, setShowGrades] = useState(false)
+    const [newTag, setNewTag] = useState('')
+    const [tags, setTags] = useState([])
 
-    return <Fragment>
-        <div className='container'>
 
-            <div className="row py-3">
-                <div className="col-sm-2 d-flex flex-column justify-content-center ">
-                    <img height="100" width='100' className='rounded-circle border border-secondary z-depth-2' src={pic} alt={firstName + ' ' + lastName} />
-                </div>
-                <div className="col-sm-8">
-                    <h3> <b>{firstName.toUpperCase() + ' ' + lastName.toUpperCase()} </b></h3>
-                    <hr className='border' />
-                    <div className='p-2'>
-                        <p>Email: {email}</p>
-                        <p>Company: {company}</p>
-                        <p>Skill: {skill}</p>
-                        <p>Average: {addReducer(grades) / grades.length}%</p>
+    if (tagSearchTerm === '') {
+
+        return <Fragment>
+            <div className='container'>
+                <div className="row py-3">
+                    <div className="col-sm-2 d-flex flex-column justify-content-center ">
+                        <img height="100" width='100' className='rounded-circle border border-secondary z-depth-2' src={pic} alt={firstName + ' ' + lastName} />
                     </div>
-                    <div className='p-2'>
-                        {showGrades 
-                        ? 
-                        <Fragment>
-                            
-                            {grades.map((grade,index) => <div> Test {index+1}:&emsp;{grade}% </div>)}
-                            
-                        </Fragment>
-                        : 
-                        null}
-                    </div>
+                    <div className="col-sm-8 pt-3">
+                        <h3> <b>{firstName.toUpperCase() + ' ' + lastName.toUpperCase()} </b></h3>
+                        <hr className='border' />
+                        <div className='p-2'>
+                            <p>Email: {email}</p>
+                            <p>Company: {company}</p>
+                            <p>Skill: {skill}</p>
+                            <p>Average: {addReducer(grades) / grades.length}%</p>
+                            {tags ?
+                                tags.map(tag => <span class="badge badge-secondary m-1 p-2">{tag}</span>)
+                                : null}
+                        </div>
+                        <div className='p-2'>
+                            {showGrades
+                                ?
+                                <Fragment>
 
-                </div>
-                <div className="col-sm-1 py-3 d-flex flex-column justify-content-start">
-                    {showGrades ? <a><i className="fas fa-minus fa-3x" onClick={() => setShowGrades(false)} ></i></a> : <i className="fas fa-plus fa-3x" onClick={() => setShowGrades(true)}></i>}
+                                    {grades.map((grade, index) => <div> Test {index + 1}:&emsp;{grade}% </div>)}
+
+                                </Fragment>
+                                :
+                                null}
+                        </div>
+
+                        <input
+                            className='p-2 form-control'
+                            type='text'
+                            placeholder='Add a tag (hit enter to create)'
+                            value={newTag}
+                            onChange={event => setNewTag(event.target.value)}
+                            onKeyDown={event => event.keyCode === 13 ? (setTags([...tags, event.target.value]), setNewTag('')) : null}
+                        />
+
+                    </div>
+                    <div className="col-sm-1 py-3 d-flex flex-column justify-content-start">
+                        {showGrades ? <a><i className="fas fa-minus fa-3x" onClick={() => setShowGrades(false)} ></i></a> : <i className="fas fa-plus fa-3x" onClick={() => setShowGrades(true)}></i>}
+                    </div>
                 </div>
             </div>
-        </div>
-    </Fragment>
+        </Fragment>
+
+    } else {
+
+        const matchingTags = tags.filter(tag => tag.includes(tagSearchTerm))
+
+        if (matchingTags.length > 0) {
+            return <Fragment>
+                <div className='container'>
+                    <div className="row py-3">
+                        <div className="col-sm-2 d-flex flex-column justify-content-center ">
+                            <img height="100" width='100' className='rounded-circle border border-secondary z-depth-2' src={pic} alt={firstName + ' ' + lastName} />
+                        </div>
+                        <div className="col-sm-8 pt-3">
+                            <h3> <b>{firstName.toUpperCase() + ' ' + lastName.toUpperCase()} </b></h3>
+                            <hr className='border' />
+                            <div className='p-2'>
+                                <p>Email: {email}</p>
+                                <p>Company: {company}</p>
+                                <p>Skill: {skill}</p>
+                                <p>Average: {addReducer(grades) / grades.length}%</p>
+                                {tags ?
+                                    tags.map(tag => <span class="badge badge-secondary m-1 p-2">{tag}</span>)
+                                    : null}
+                            </div>
+                            <div className='p-2'>
+                                {showGrades
+                                    ?
+                                    <Fragment>
+
+                                        {grades.map((grade, index) => <div> Test {index + 1}:&emsp;{grade}% </div>)}
+
+                                    </Fragment>
+                                    :
+                                    null}
+                            </div>
+
+                            <input
+                                className='p-2 form-control'
+                                type='text'
+                                placeholder='Add a tag (hit enter to create)'
+                                value={newTag}
+                                onChange={event => setNewTag(event.target.value)}
+                                onKeyDown={event => event.keyCode === 13 ? (setTags([...tags, event.target.value]), setNewTag('')) : null}
+                            />
+
+                        </div>
+                        <div className="col-sm-1 py-3 d-flex flex-column justify-content-start">
+                            {showGrades ? <a><i className="fas fa-minus fa-3x" onClick={() => setShowGrades(false)} ></i></a> : <i className="fas fa-plus fa-3x" onClick={() => setShowGrades(true)}></i>}
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
+        }
+        else {
+            return null
+        }
+
+    }
+
+
+
+
 }
 
 export default Student
--- client/src/components/StudentDirectory.js
+++ client/src/components/StudentDirectory.js
@@ -5,12 +5,11 @@ const StudentDirectory = () => {
 
     const [students, setStudents] = useState([])
     const [nameSearchTerm, setNameSearchTerm] = useState('')
-    // const [tagSearchTerm, setTagSearchTerm] = useState('')
+    const [tagSearchTerm, setTagSearchTerm] = useState('')
 
     const getData = async () => {
         const response = await fetch('https://api.hatchways.io/assessment/students')
         const payload = await response.json()
-        console.log(payload)
         setStudents(payload.students)
     }
 
@@ -22,10 +21,22 @@ const StudentDirectory = () => {
 
     return <Fragment>
         <div className="card my-5 p-3 rounded w-75 mx-auto" id='directory'>
-            <input className='p-2 my-1 w-100' type='text' placeholder='Search by name' value={nameSearchTerm} onChange={(event)=>setNameSearchTerm(event.target.value)}/>
-            <input className='p-2 my-1 w-100' type='text' placeholder='Search by tag' />
+            <input
+                className='p-2 my-1 w-100 form-control'
+                type='text'
+                placeholder='Search by name'
+                value={nameSearchTerm}
+                onChange={(event) => setNameSearchTerm(event.target.value)}
+            />
+            <input
+                className='p-2 my-1 w-100 form-control'
+                type='text'
+                placeholder='Search by tag'
+                value={tagSearchTerm}
+                onChange={(event) => setTagSearchTerm(event.target.value)}
+            />
             <ul className="list-group list-group-flush">
-                {studentsFiltered.map(student => <li className="list-group-item" key={student.id}> <Student key={student.id} {...student} /> </li>)}
+                {studentsFiltered.map(student => <li className="list-group-item" key={student.id}> <Student key={student.id} {...student} tagSearchTerm={tagSearchTerm}/> </li>)}
             </ul>
         </div>
     </Fragment>
